c1b5a653db1a47fb282cf86102171f3a6807769b
AnandhuU7 <anandhuachu777.com>
2025-02-14 16:53:31 +0530

    Added new page and issue with preview fixed

--- dist/chunks/package-2_5vFrgb2L.mjs
+++
@@ -1,8 +0,0 @@
-async function getMod() {
-						return import('./package-2_CobSbW_Y.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts: [] };
-
-export { defaultMod as default };
