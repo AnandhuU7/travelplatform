7944a6ec9193b0efc4109ff1b0c27573e8c8a0f6
AnandhuU7 <anandhuachu777.com>
2025-02-19 18:21:52 +0530

    Structure updated

---
+++ dist/chunks/package-5_BP7X9wt4.mjs
@@ -0,0 +1,223 @@
+import { l as createVNode, F as Fragment, _ as __astro_tag_component__ } from './astro/server_CIkRzRWx.mjs';
+import 'clsx';
+
+const frontmatter = {
+  "title": "5D5N Uzbekistan Travel",
+  "slug": "package-5",
+  "date": "2024-12-01",
+  "author": "Futourgo Travel",
+  "description": "Experience the beauty and history of Uzbekistan with this 5-night, 5-day travel package. Visit Tashkent, Samarkand, Bukhara, and more.",
+  "cardImage": "/src/assets/serviceimg/service28.jpg",
+  "cardImageAlt": "Travel-image",
+  "price": 1800,
+  "currency": "USD",
+  "location": "Uzbekistan",
+  "pdfUrl": "/assets/pdf/5N5D_UZB.pdf",
+  "duration": "5 Nights 5 Days",
+  "inclusions": ["5 nights accommodation in 3/4-star hotels with breakfast.", "Airport pickup and drop-off.", "All transfers via premium van.", "English & Malayalam-speaking guides.", "Entry tickets to attractions as per itinerary.", "Experience the Soviet-era metro line in Uzbekistan.", "One complimentary national lunch at the world-famous Plov Center.", "Cultural activities and sightseeing."],
+  "route": "/contact",
+  "id": 5,
+  "publishedTime": "2024-12-01",
+  "modifiedTime": "2024-12-01"
+};
+function getHeadings() {
+  return [{
+    "depth": 2,
+    "slug": "5-nights-5-days-uzbekistan-tour-package",
+    "text": "5 Nights 5 Days Uzbekistan Tour Package"
+  }, {
+    "depth": 3,
+    "slug": "day-1-arrival-in-tashkent",
+    "text": "Day 1: Arrival in Tashkent"
+  }, {
+    "depth": 3,
+    "slug": "day-2-tashkent-to-samarkand",
+    "text": "Day 2: Tashkent to Samarkand"
+  }, {
+    "depth": 3,
+    "slug": "day-3-samarkand-to-bukhara",
+    "text": "Day 3: Samarkand to Bukhara"
+  }, {
+    "depth": 3,
+    "slug": "day-4-bukhara-to-tashkent",
+    "text": "Day 4: Bukhara to Tashkent"
+  }, {
+    "depth": 3,
+    "slug": "day-5-tashkent---amirsoy--departure",
+    "text": "Day 5: Tashkent - Amirsoy & Departure"
+  }, {
+    "depth": 2,
+    "slug": "total-package-price-1800-usd",
+    "text": "Total Package Price: 1800 USD"
+  }];
+}
+function _createMdxContent(props) {
+  const _components = {
+    a: "a",
+    em: "em",
+    h2: "h2",
+    h3: "h3",
+    li: "li",
+    p: "p",
+    strong: "strong",
+    ul: "ul",
+    ...props.components
+  };
+  return createVNode(Fragment, {
+    children: [createVNode(_components.h2, {
+      id: "5-nights-5-days-uzbekistan-tour-package",
+      children: "5 Nights 5 Days Uzbekistan Tour Package"
+    }), "\n", createVNode(_components.p, {
+      children: createVNode(_components.em, {
+        children: "(Date: 01/12/24 – 06/12/24)"
+      })
+    }), "\n", createVNode(_components.h3, {
+      id: "day-1-arrival-in-tashkent",
+      children: "Day 1: Arrival in Tashkent"
+    }), "\n", createVNode(_components.ul, {
+      children: ["\n", createVNode(_components.li, {
+        children: createVNode(_components.strong, {
+          children: "Arrive at Tashkent International Airport."
+        })
+      }), "\n", createVNode(_components.li, {
+        children: createVNode(_components.strong, {
+          children: "Transfer to hotel and check-in."
+        })
+      }), "\n", createVNode(_components.li, {
+        children: createVNode(_components.strong, {
+          children: "Visit Khast Imam Complex, Chorsu Bazaar, and Old City."
+        })
+      }), "\n", createVNode(_components.li, {
+        children: createVNode(_components.strong, {
+          children: "Explore Independence Square, Amir Timur Square, and the Museum of Applied Arts."
+        })
+      }), "\n", createVNode(_components.li, {
+        children: createVNode(_components.strong, {
+          children: "Overnight stay in Tashkent."
+        })
+      }), "\n"]
+    }), "\n", createVNode(_components.h3, {
+      id: "day-2-tashkent-to-samarkand",
+      children: "Day 2: Tashkent to Samarkand"
+    }), "\n", createVNode(_components.ul, {
+      children: ["\n", createVNode(_components.li, {
+        children: createVNode(_components.strong, {
+          children: "Take the high-speed train to Samarkand (approx. 2 hours)."
+        })
+      }), "\n", createVNode(_components.li, {
+        children: createVNode(_components.strong, {
+          children: "Visit Registan, Gur-e-Amir Mausoleum, and Bibi-Khanym Mosque."
+        })
+      }), "\n", createVNode(_components.li, {
+        children: createVNode(_components.strong, {
+          children: "Explore Shah-i-Zinda Necropolis and Siyob Bazaar."
+        })
+      }), "\n", createVNode(_components.li, {
+        children: createVNode(_components.strong, {
+          children: "Overnight stay in Samarkand."
+        })
+      }), "\n"]
+    }), "\n", createVNode(_components.h3, {
+      id: "day-3-samarkand-to-bukhara",
+      children: "Day 3: Samarkand to Bukhara"
+    }), "\n", createVNode(_components.ul, {
+      children: ["\n", createVNode(_components.li, {
+        children: createVNode(_components.strong, {
+          children: "Take the high-speed train to Bukhara."
+        })
+      }), "\n", createVNode(_components.li, {
+        children: createVNode(_components.strong, {
+          children: "Visit Lyab-i-Hauz Ensemble, Kalyan Minaret, and Ark of Bukhara."
+        })
+      }), "\n", createVNode(_components.li, {
+        children: createVNode(_components.strong, {
+          children: "Explore Po-i-Kalyan Mosque, Trading Domes, and Old City."
+        })
+      }), "\n", createVNode(_components.li, {
+        children: createVNode(_components.strong, {
+          children: "Overnight stay in Bukhara."
+        })
+      }), "\n"]
+    }), "\n", createVNode(_components.h3, {
+      id: "day-4-bukhara-to-tashkent",
+      children: "Day 4: Bukhara to Tashkent"
+    }), "\n", createVNode(_components.ul, {
+      children: ["\n", createVNode(_components.li, {
+        children: createVNode(_components.strong, {
+          children: "Full day dedicated to exploring Bukhara’s rich historical sites."
+        })
+      }), "\n", createVNode(_components.li, {
+        children: createVNode(_components.strong, {
+          children: "Visit Ark Fortress, Kalon Minaret, Chor Minor Madrasah, Bolo Hauz Mosque, Mir-i-Arab Madrasa, and Ulugʻbek Madrasa."
+        })
+      }), "\n", createVNode(_components.li, {
+        children: createVNode(_components.strong, {
+          children: "Evening train at 4:41 PM to Tashkent."
+        })
+      }), "\n", createVNode(_components.li, {
+        children: createVNode(_components.strong, {
+          children: "Overnight stay in Tashkent."
+        })
+      }), "\n"]
+    }), "\n", createVNode(_components.h3, {
+      id: "day-5-tashkent---amirsoy--departure",
+      children: "Day 5: Tashkent - Amirsoy & Departure"
+    }), "\n", createVNode(_components.ul, {
+      children: ["\n", createVNode(_components.li, {
+        children: createVNode(_components.strong, {
+          children: "Drive to Amirsoy Mountain Resort (approx. 2 hours)."
+        })
+      }), "\n", createVNode(_components.li, {
+        children: createVNode(_components.strong, {
+          children: "Optional hike to the top of Amirsoy Mountain."
+        })
+      }), "\n", createVNode(_components.li, {
+        children: createVNode(_components.strong, {
+          children: "Visit Chimgan Mountains for scenic views."
+        })
+      }), "\n", createVNode(_components.li, {
+        children: createVNode(_components.strong, {
+          children: "Enjoy activities at Charvak Lake (boat rides, swimming, relaxation)."
+        })
+      }), "\n", createVNode(_components.li, {
+        children: createVNode(_components.strong, {
+          children: "Return to Tashkent and transfer to the airport at 10 PM."
+        })
+      }), "\n"]
+    }), "\n", createVNode(_components.h2, {
+      id: "total-package-price-1800-usd",
+      children: "Total Package Price: 1800 USD"
+    }), "\n", createVNode(_components.p, {
+      children: createVNode(_components.strong, {
+        children: "(Can be customized based on preferences.)"
+      })
+    }), "\n", createVNode(_components.p, {
+      children: ["Looking for more than just a trip? Begin your journey with us at ", createVNode(_components.a, {
+        href: "https://www.futourgo.com",
+        children: "Futourgo"
+      }), "."]
+    })]
+  });
+}
+function MDXContent(props = {}) {
+  const {wrapper: MDXLayout} = props.components || ({});
+  return MDXLayout ? createVNode(MDXLayout, {
+    ...props,
+    children: createVNode(_createMdxContent, {
+      ...props
+    })
+  }) : _createMdxContent(props);
+}
+
+const url = "src/content/packages/package-5.mdx";
+const file = "C:/Users/anand/OneDrive/Desktop/proje/travelplatform/src/content/packages/package-5.mdx";
+const Content = (props = {}) => MDXContent({
+  ...props,
+  components: { Fragment: Fragment, ...props.components, },
+});
+Content[Symbol.for('mdx-component')] = true;
+Content[Symbol.for('astro.needsHeadRendering')] = !Boolean(frontmatter.layout);
+Content.moduleId = "C:/Users/anand/OneDrive/Desktop/proje/travelplatform/src/content/packages/package-5.mdx";
+__astro_tag_component__(Content, 'astro:jsx');
+
+export { Content, Content as default, file, frontmatter, getHeadings, url };
