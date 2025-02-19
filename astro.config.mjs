dd09b6c34d8851f2eaa8ce3ff1d987884af58217
AnandhuU7 <anandhuachu777.com>
2025-02-19 11:13:52 +0530

    SEO Added

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,17 @@ import { defineConfig } from "astro/config";
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
-
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), mdx({
-    syntaxHighlight: 'shiki',
-    shikiConfig: { theme: 'dracula' },
-    
-   
-    remarkRehype: { footnoteLabel: 'Footnotes' },
-    gfm: false,
-  })]
+  site: 'https://travel.digiimpact.in',
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    mdx({
+      syntaxHighlight: 'shiki',
+      shikiConfig: { theme: 'dracula' },
+      remarkRehype: { footnoteLabel: 'Footnotes' },
+      gfm: false,
+    }),
+  ],
 });
