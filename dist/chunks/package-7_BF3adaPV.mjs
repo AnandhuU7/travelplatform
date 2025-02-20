036174616ce7131b55b458fab566bee7a808e190
AnandhuU7 <anandhuachu777.com>
2025-02-20 10:53:18 +0530

    Terms responsive

---
+++ dist/chunks/package-7_BF3adaPV.mjs
@@ -0,0 +1,8 @@
+async function getMod() {
+						return import('./package-7_BMEP05cO.mjs');
+					}
+					const collectedLinks = [];
+					const collectedStyles = [];
+					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts: [] };
+
+export { defaultMod as default };
