d7f0bf5a2b2b2aec2e763f64a3ec6dc11b0928a1
nibhapr <nibhapr@gmail.com>
2025-03-12 12:54:37 +0530

    new build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@ import { defineConfig } from "astro/config";
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import robotsTxt from 'astro-robots-txt';
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: 'https://travelplatformuae.com',
 
   integrations: [
     robotsTxt(),
+    sitemap(),
     tailwind({
       applyBaseStyles: false,
     }),
